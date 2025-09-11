984b47a1f06cabe4a181d7bd92e7f545cf513cb3
JourBoon <tom.caillaud85@gmail.com>
2025-09-11 16:25:22 +0200

    console.go

--- console.go
+++ console.go
@@ -4,18 +4,40 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+	"unicode"
+	"strings"
+	"strconv"
 )
 
 // func main() {
-// 	badam := askPlayer("Comment vous appelez-vous ?")
-// 	fmt.Println("answer : ", badam)
-// 	age := askPlayer("age ?")
-// 	fmt.Println(age)
+//  	badam := askPlayer("Comment vous appelez-vous ?")
+//  	fmt.Println("answer : ", badam)
+// 	fmt.Printf("%T\n", badam)
+//  	age := askPlayer("age ?")
 // }
 
 func askPlayer(question string) interface{} {
 	reader := bufio.NewReader(os.Stdin)
-    fmt.Print(question, " : ")
-    text, _ := reader.ReadString('\n')
+	fmt.Print(question, " : ")
+	text, _ := reader.ReadString('\n')
+	text = strings.TrimSpace(text)
+
+	if isInt(text) {
+		if number, err := strconv.Atoi(text); err == nil {
+			return number
+		}
+	}
 	return text
+}
+
+func isInt(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+	for _, c := range s {
+		if !unicode.IsDigit(c) {
+			return false
+		}
+	}
+	return true
 }
