f2e9daab804c2950892ca633c97249eb0bf1c162
WayeNot <aymericbeaune917@gmail.com>
2025-09-17 15:55:33 +0200

    Modif items + useItem

--- game/inventory.go
+++ game/inventory.go
@@ -23,46 +23,49 @@ type Item struct {
 	AddPvMax				int
 	PtsAttack				int
 	IsForgeron 				bool
-	IsUsable 				bool
+	IsUsableInGame 			bool
 	CancelPoison			bool
 	RessourceForForgeron 	bool
 	itemNeeded				int
 	itemNeededQuantity		int
 	RewardIG				bool
+	canLooseItem 			int
 }
 
 var allItems = map[int]Item{
 
 	// ------------------- Ressources Marchand ↓
 
-	1: {Id: 1, Name: "Pain sec", Price: 2, Icon: "🍞", IsForgeron: false, AddHealth: 5, IsUsable: true},
-	2: {Id: 2, Name: "Canette d’Oasis tiède", Price: 4, Icon: "🍺", IsForgeron: false, AddHealth: 10, IsUsable: true},
-	3: {Id: 3, Name: "Kebab du Destin", Price: 15, Icon: "🍔", IsForgeron: false, AddHealth: 100, IsUsable: true},
+	1: {Id: 1, Name: "Pain sec", Price: 2, Icon: "🍞", IsForgeron: false, AddHealth: 5, IsUsableInGame: true},
+	2: {Id: 2, Name: "Kebab du Destin", Price: 15, Icon: "🍔", IsForgeron: false, AddHealth: 15, IsUsableInGame: true},
 
-	4: {Id: 4, Name: "Stylo BIC", Price: 3, Icon: "🖊", IsForgeron: false, IsUsable: true},
-	5: {Id: 5, Name: "Formulaire Cerfa 666-B", Price: 6, Icon: "📄", IsForgeron: false, RessourceForForgeron: true},
-	6: {Id: 6, Name: "Carte Navigo périmée", Price: 1, Icon: "🎫", IsForgeron: false, RessourceForForgeron: true},
+	3: {Id: 3, Name: "8.6 tiède", Price: 4, Icon: "🍺", IsForgeron: false, AddHealth: 5, IsUsableInGame: true},
+	4: {Id: 4, Name: "Canette de Kronenbourg", Price: 10, Icon: "🥤", IsForgeron: false, AddHealth: 10, IsUsableInGame: true},
 
-	7: {Id: 7, Name: "Sac à dos troué", Price: 20, Icon: "🎒", IsForgeron: false, giveInventory: 5, IsUsable: true},
-	8: {Id: 8, Name: "Sac à dos Décathlon", Price: 35, Icon: "🎒", IsForgeron: false, giveInventory: 10, IsUsable: true},
+	5: {Id: 5, Name: "Stylo BIC", Price: 3, Icon: "🖊", IsForgeron: false, IsUsableInGame: true},
+	6: {Id: 6, Name: "Formulaire Cerfa 666-B", Price: 6, Icon: "📄", IsForgeron: false, RessourceForForgeron: true},
+	7: {Id: 7, Name: "Carte Navigo périmée", Price: 1, Icon: "🎫", IsForgeron: false, RessourceForForgeron: true},
+	8: {Id: 8, Name: "CV Légendaire", Price: 20, Icon: "📃", IsForgeron: true, PtsAttack: 10, itemNeeded: 4, itemNeededQuantity: 1}, // stylo BIC
+
+	9: {Id: 9, Name: "Sac à dos troué", Price: 20, Icon: "🎒", IsForgeron: false, giveInventory: 5, IsUsableInGame: false, canLooseItem: 20},
+	10: {Id: 10, Name: "Sac à dos Décathlon", Price: 35, Icon: "🎒", IsForgeron: false, giveInventory: 10, IsUsableInGame: false},
 
 	// Ressources de craft pour le Forgeron ↓
 
-	9: {Id: 9, Name: "Laine de Chèvre", Price: 5, Icon: "🐐", IsForgeron: false, RessourceForForgeron: true, RewardIG: true},
-	10: {Id: 10, Name: "Attestation Pôle-Emploi", Price: 7, Icon: "📑", IsForgeron: false, RessourceForForgeron: true, RewardIG: true},
-	11: {Id: 11, Name: "Ticket resto usagé", Price: 2, Icon: "🎟", IsForgeron: false, RessourceForForgeron: true, RewardIG: true},
-	12: {Id: 12, Name: "Badge CGT", Price: 10, Icon: "👑", IsForgeron: false, RessourceForForgeron: true, RewardIG: true},
-	13: {Id: 13, Name: "Lingot de SMIC", Price: 12, Icon: "💰", IsForgeron: false, RessourceForForgeron: true, RewardIG: true},
+	11: {Id: 11, Name: "Laine de Chèvre", Price: 5, Icon: "🐐", IsForgeron: false, RessourceForForgeron: true, RewardIG: true},
+	12: {Id: 12, Name: "Attestation Pôle-Emploi", Price: 7, Icon: "📑", IsForgeron: false, RessourceForForgeron: true, RewardIG: true},
+	13: {Id: 13, Name: "Ticket resto usagé", Price: 2, Icon: "🎟", IsForgeron: false, RessourceForForgeron: true, RewardIG: true},
+	14: {Id: 14, Name: "Badge CGT", Price: 10, Icon: "👑", IsForgeron: false, RessourceForForgeron: true, RewardIG: true},
+	15: {Id: 15, Name: "Lingot de SMIC", Price: 12, Icon: "💰", IsForgeron: false, RessourceForForgeron: true, RewardIG: true},
 
 	// ------------------- Ressources Forgeron ↓
 
-	14: {Id: 14, Name: "Casquette du Chômeur", Price: 5, Icon: "🧢", IsForgeron: true, AddPvMax: 10, itemNeeded: 9, itemNeededQuantity: 1}, // laine de chèvre
-	15: {Id: 15, Name: "Costume d’Entretien Froissé", Price: 15, Icon: "🤵", IsForgeron: true, AddPvMax: 25, itemNeeded: 10, itemNeededQuantity: 1}, // attestation
-	16: {Id: 16, Name: "Chaussures de Sécurité Abîmées", Price: 8, Icon: "🥾", IsForgeron: true, AddPvMax: 15, itemNeeded: 11, itemNeededQuantity: 1}, // ticket resto
+	16: {Id: 16, Name: "Casquette du Chômeur", Price: 15, Icon: "🧢", IsForgeron: true, AddPvMax: 5, itemNeeded: 9, itemNeededQuantity: 1}, // laine de chèvre
+	17: {Id: 17, Name: "Costume d’Entretien Froissé", Price: 20, Icon: "🤵", IsForgeron: true, AddPvMax: 10, itemNeeded: 10, itemNeededQuantity: 1}, // attestation
+	18: {Id: 18, Name: "Chaussures de Sécurité Abîmées", Price: 30, Icon: "🥾", IsForgeron: true, AddPvMax: 15, itemNeeded: 11, itemNeededQuantity: 1}, // ticket resto
 
-	17: {Id: 17, Name: "CV Légendaire", Price: 20, Icon: "📃", IsForgeron: true, PtsAttack: 10, itemNeeded: 4, itemNeededQuantity: 1}, // stylo BIC
-	18: {Id: 18, Name: "Épée en SMIC", Price: 25, Icon: "🗡️", IsForgeron: true, PtsAttack: 15, itemNeeded: 13, itemNeededQuantity: 1}, // lingot de SMIC
-	19: {Id: 19, Name: "Arc de Syndicaliste", Price: 18, Icon: "🏹", IsForgeron: true, PtsAttack: 12, itemNeeded: 12, itemNeededQuantity: 1}, // badge CGT
+	19: {Id: 19, Name: "Épée en SMIC", Price: 25, Icon: "🗡️", IsForgeron: true, PtsAttack: 15, itemNeeded: 13, itemNeededQuantity: 1}, // lingot de SMIC
+	20: {Id: 20, Name: "Arc de Syndicaliste", Price: 18, Icon: "🏹", IsForgeron: true, PtsAttack: 12, itemNeeded: 12, itemNeededQuantity: 1}, // badge CGT
 }
 
 func (c Character) GetItemNumber() int {
@@ -110,14 +113,12 @@ func (c *Character) RemoveItem(itemId, itemQuantity int) {
 }
 
 func (c Character) AccessInventory() {
-	inv := c.Inventory
-
 	for {
-		if len(inv) > 0 {
+		if len(c.Inventory) > 0 {
 			fmt.Println("-------------------------------------")
 			fmt.Println(" Votre Inventaire : (", c.GetItemNumber(), " / ", c.MaxInventory, " ) ")
 			fmt.Println("-------------------------------------")
-			for k, v := range inv {
+			for k, v := range c.Inventory {
 				item := allItems[v.Id]
 				fmt.Println(k + 1,") Item : ", item.Icon, " | ", item.Name, "x", v.Quantity, "/ Prix :", item.Price, "₣")
 			}
@@ -126,11 +127,13 @@ func (c Character) AccessInventory() {
 			fmt.Println("Aucun item pour le moment !")
 			fmt.Println("---------------------------")
 		}
-		ChoiceUser := AskPlayerInt("Tapez 0 pour revenir au menu principal")
+		ChoiceUser := AskPlayerInt("Tapez le numéro d'un item pour intéragir avec (0 pour annuler) : ")
 
 		if ChoiceUser == 0 {
 			return
 		}
+
+		// if allItems[]
 	}
 }
 
@@ -141,11 +144,6 @@ func (c *Character) UseItem(itemId, q int) {
 	}
 
 	item := allItems[itemId]
-
-	if !item.IsUsable {
-		fmt.Println("Cet item n'est pas utilisable !")
-		return
-	}
 
 	if item.TimeUsing > 0 {
 		time.Sleep(time.Duration(item.TimeUsing))
@@ -166,9 +164,13 @@ func (c *Character) UseItem(itemId, q int) {
 		fmt.Printf("Vous avez %v points de vies supplémentaires !", item.AddPvMax)
 	}
 
+	if item.canLooseItem > 0 {
+		fmt.Println("Vous avez perdu un item de votre sac à dos troué")
+	}
+
 	if item.giveInventory > 0 {
 		c.MaxInventory += item.giveInventory
-		fmt.Printf("Votre inventaire augmente de %v slots", item.giveInventory)
+		fmt.Printf("Vous avez équipé un sac à dos, votre inventaire est passé à %v slots", c.MaxInventory + item.giveInventory)
 	}
 
 	if item.removeEnemyHealth > 0 {
